cde3e15e99f1223fda9a3b606330abdbd1b6a861
BrayanDH <bdhdavid.bh@gmail.com>
2025-09-10 21:23:58 -0600

    agregado poscicion

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -32,6 +32,13 @@ const Navbar: React.FC = () => {
             display: flex;
             justify-content: space-between;
             align-items: center;
+            position: fixed;
+            top: 0;
+            left: 0;
+            right: 0;
+            z-index: 1000;
+            width: 100%;
+            box-sizing: border-box;
           }
 
           .navbar-list {
